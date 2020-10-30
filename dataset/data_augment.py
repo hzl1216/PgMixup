5971867df7e144a99917650387f57237138a65a9
2101975298@qq.com <hzl1216>
2020-10-30 19:01:22 +0800

    'update'

--- dataset/data_augment.py
+++ dataset/data_augment.py
@@ -1,14 +1,14 @@
-from dataset.autoaugment import RandAugment
+from .autoaugment import CIFAR10Policy,Cutout,SVHNPolicy
 import torchvision.transforms as transforms
 def get_data_augment(dataset):
     if dataset == 'cifar10':
         means = (0.4914, 0.4822, 0.4465)
         stds = (0.2471, 0.2435, 0.2616)
-        transform_aug = transforms.Compose([
-             RandAugment(3, 5),
-             transforms.RandomCrop(32, padding=4),  # fill parameter needs torchvision installed from source
-             transforms.RandomHorizontalFlip(),
+        transform_aug = transforms.Compose(
+            [transforms.RandomCrop(32, padding=4),  # fill parameter needs torchvision installed from source
+             transforms.RandomHorizontalFlip(), CIFAR10Policy(),
              transforms.ToTensor(),
+             Cutout(n_holes=1, length=16),  # (https://github.com/uoguelph-mlrg/Cutout/blob/master/util/cutout.py)
              transforms.Normalize(means, stds),
              ])
         transform_normal = transforms.Compose([
@@ -27,10 +27,9 @@ def get_data_augment(dataset):
         means = (0.4376821, 0.4437697, 0.47280442)
         stds = (0.19803012, 0.20101562, 0.19703614)
         transform_aug = transforms.Compose([
-            RandAugment(3, 7),
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
+            SVHNPolicy(),
             transforms.ToTensor(),
+            Cutout(n_holes=1, length=20),
             transforms.Normalize(means, stds),
         ])
         transform_normal = transforms.Compose([
