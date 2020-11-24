ab26d054b8e8fe34d13ddbd0c3ff8303e521758c
2101975298@qq.com <hzl1216>
2020-11-24 15:07:15 +0800

    update

--- scripts/cifar10_250_labels.sh
+++ scripts/cifar10_250_labels.sh
@@ -1,1 +1,1 @@
-python train.py --dataset=cifar10 --epochs=200  --optimizer='sgd' --consistency-weight=6.0 --lr=0.03 --weight-decay=0.0007  --n-labeled=250 --gpu=1 --batch-size=16 --unsup-ratio=20
+python train.py --dataset=cifar10 --epochs=150  --optimizer='sgd' --consistency-weight=6.0 --lr=0.03 --weight-decay=0.0007  --n-labeled=250 --gpu=1 --batch-size=16 --unsup-ratio=20
