65394c2ece80ff5465f97355751d8c11f305f377
2101975298@qq.com <hzl1216>
2020-11-30 10:16:13 +0800

    enlarge consistency weight

--- scripts/cifar10_250.sh
+++ scripts/cifar10_250.sh
@@ -1,1 +1,1 @@
-python train.py --dataset=cifar10 --epochs=200  --optimizer='sgd' --consistency-weight=1.0 --lr=0.03 --weight-decay=0.0007  --n-labeled=250 --gpu=1 --batch-size=16 --unsup-ratio=20
+python train.py --dataset=cifar10 --epochs=200  --optimizer='sgd' --consistency-weight=8.0 --lr=0.03 --weight-decay=0.0005  --n-labeled=250 --gpu=1 --batch-size=16 --unsup-ratio=20
